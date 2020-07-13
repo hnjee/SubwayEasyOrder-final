9289b2d2196a23dce971baf930afc8097c0092cb
SOOKYOUNG-KIM <63095409+SOOKYOUNG-KIM@users.noreply.github.com>
2020-07-13 12:42:30 +0900

    survey 수정

---
+++ src/main/java/com/subway/s1/survey/chart/SurveyChartRepository.java
@@ -0,0 +1,10 @@
+package com.subway.s1.survey.chart;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface SurveyChartRepository {
+
+}
