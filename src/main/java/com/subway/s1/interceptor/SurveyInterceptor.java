b0b75a40264263f15ff6df63c46a9768c959e773
moonlight380 <63095222+moonlight380@users.noreply.github.com>
2020-07-21 09:47:51 +0900

    survey Interceptor

---
+++ src/main/java/com/subway/s1/interceptor/SurveyInterceptor.java
@@ -0,0 +1,41 @@
+package com.subway.s1.interceptor;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.subway.s1.member.MemberVO;
+
+@Component
+public class SurveyInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		boolean check = false;
+		
+		MemberVO memberVO = (MemberVO)request.getSession().getAttribute("member");
+		
+		if(memberVO != null &&( memberVO.getLevel()==2|| memberVO.getLevel()==1 )) {
+			check = true;
+		}else {
+			request.setAttribute("result", "가맹점모드 입니다.");
+			request.setAttribute("path", "../");
+			System.out.println("member"+memberVO.getId()+memberVO.getLevel());
+			RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/common/result.jsp"); 
+			view.forward(request, response);
+		}
+		
+	
+		return check;
+	}
+	
+
+}
+
+
